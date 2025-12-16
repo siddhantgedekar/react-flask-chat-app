9fb99494577d78dd9fb62a50ea942f59d3641fb6
siddhantgedekar <sapwoodg18@gmail.com>
2025-12-16 15:27:57 +0530

    Updated AI code making it more responsive and able to remember different users

--- backend/app.py
+++ backend/app.py
@@ -4,13 +4,25 @@ from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 from flask_session import Session
 from dotenv import load_dotenv
-import ollama
+import google.generativeai as genai
+import os
+
+# Switching from local AI to Google's cloud AI
+# 1. load the secret key from .env file
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+
+# 2. configure AI
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel('gemini-2.5-flash') # A fast, efficient model
+
+# Implementing memory for context retention
+chat_session = model.start_chat(history=[])
 
 # create Flask app
 app = Flask(__name__)
-# CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}}) # adjust origin as needed
+
 # configure security key
-app.config['SECRET_KEY'] = "mysecretkey" # in production, load from environment variable
 CORS(app) # enable CORS for all origins
 
 socketio = SocketIO(app, cors_allowed_origins="*") # initialize socketio
@@ -35,39 +47,21 @@ def home():
 def chat():
     data = request.json
     user_message = data.get('message', '')
+    user_id = data.get('user_id')
+
+    if user_id not in user_sessions:
+        # create if not
+        user_sessions[user_id] = model.start_chat(history=[])
     
-    if not user_message:
-        return jsonify({'error': 'No message provided.'}), 400
+    # get user's chat sessions
+    current_session = user_sessions[user_id]
+
+    # ask gemini a question
+    response = current_session.send_message(user_message)
+
+    bot_reply = response.text
     
-    if 'history' not in session:
-        session['history'] = []
-
-    try:
-        # append user message to conversation history
-        session['history'].append({"role": "user", "content": user_message})
-
-        messages = [{"role": "user", "content": user_message}]
-
-        print(f"--- 2. Sending to Ollama: {user_message} ---") # Checkpoint 2
-
-        # send request to AI model
-        response = ollama.chat(model=model_name, messages=session['history'])
-
-        print("--- 3. Ollama Finished! ---") # Checkpoint 3
-
-        # extract reply
-        bot_reply = response['message']['content']
-
-        # append bot reply to conversation history
-        session['history'].append({"role": "bot", "content": bot_reply})
-
-        session.modified = True  # mark session as modified to save changes
-        
-        return jsonify({'reply': bot_reply})
-    
-    except Exception as e:
-        print(f'Error: {e}')
-        return jsonify({'error': 'An error occurred while processing your request.'}), 500
+    return jsonify({'reply': bot_reply})
 
 # global chat endpoint
 @socketio.on('connect')
