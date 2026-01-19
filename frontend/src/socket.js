a65a9cb8f7654c651e4025b1bfd54651ef28a1e2
siddhantgedekar <siddhantgedekar.sits.comp@gmail.com>
2026-01-19 13:08:23 +0530

    fixing server

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -5,5 +5,5 @@ import { io } from "socket.io-client";
 export const BACKEND_URL = import.meta.env.PROD ? 'https://sids-worldchat.onrender.com' : ''; // for PROD vs DEV environment
 export const socket = io(BACKEND_URL || "http://localhost:5000", {
     autoConnect: false,
-    transports: ['websocket']
+    // transports: ['websocket']
 });
